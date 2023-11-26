3cdc40afa178b1b292333fb748ef5c33daf483e8
Abhinav <66725911+Abhinav16072001@users.noreply.github.com>
2023-11-26 15:44:28 +0530

    Video 9

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,7 +7,13 @@ function App() {
     <>
       <Navbar title="Word Counter" aboutText="About" />
       <div className="container">
-        <TextForm heading="Enter text to analyze" button1="Convert to UpperCase" button2="Convert to LowerCase" button3="Clear"/>
+        <TextForm
+          heading="Enter text to analyze"
+          button1="Convert to UpperCase"
+          button2="Convert to LowerCase"
+          button3="Clear"
+          button4="Copy Text"
+        />
       </div>
     </>
   );
