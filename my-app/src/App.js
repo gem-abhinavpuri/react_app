6144c68e0cfbf8037f6f93c49b1cc8e0ab6a1e03
Abhinav <66725911+Abhinav16072001@users.noreply.github.com>
2023-11-26 16:21:26 +0530

    video 10

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,19 +1,22 @@
 import "./App.css";
+import AboutUs from "./components/AboutUs";
 import Navbar from "./components/Navbar";
-import TextForm from "./components/TextForm";
+// import TextForm from "./components/TextForm";
 
 function App() {
   return (
     <>
       <Navbar title="Word Counter" aboutText="About" />
       <div className="container">
-        <TextForm
+        {/* <TextForm
           heading="Enter text to analyze"
           button1="Convert to UpperCase"
           button2="Convert to LowerCase"
           button3="Clear"
           button4="Copy Text"
-        />
+        /> */}
+
+        <AboutUs />
       </div>
     </>
   );
